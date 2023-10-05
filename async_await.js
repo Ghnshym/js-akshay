dee60e3dd60b022188fe953e30bd8d0465c933c8
Ghanshyam kumar <bca1902113@gmail.com>
2023-10-05 12:59:53 +0530

    22th

--- async_await.js
+++ async_await.js
@@ -61,3 +61,28 @@
 // }
 
 // getData();
+
+// another example 
+// Define an asynchronous function
+
+const API_URL = 'https://api.github.com/users/ghnshym';
+
+async function fetchData() {
+    try {
+      const response = await fetch(API_URL);
+      
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      
+      const data = await response.json();
+      
+      // Do something with the data
+      console.log('Data:', data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  
+  fetchData();
+  
